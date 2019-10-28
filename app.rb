af8336517694c4690d0d040d455d21ef23dff5de
Johnnae Tiffany <jrtiff0221@gmail.com>
2019-10-28 20:07:46 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,12 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   configure do
   enable :sessions
-  set :session_secret, "secret"
+  set :session_secret, "shopping_session"
   end
+
+  get '/' do
+    erb :index
+  end
+
+  
 end
